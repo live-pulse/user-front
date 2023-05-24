c385337b376e9906fc7073cd911e2f9327b2259c
Hannah <ghdcoalss33@gmail.com>
2023-05-24 16:29:58 +0900

    feat(navbar): main navbar 디테일 업

--- components/navber/MainNavbar.tsx
+++ components/navber/MainNavbar.tsx
@@ -2,8 +2,23 @@ import styled from 'styled-components';
 import { realCameraIcon, userIcon, plusIcon } from '@/components/svgs/Svgs';
 import Menubar from '@/components/navber/Menubar';
 import Link from 'next/link';
+import { deleteCookie, getCookie } from 'cookies-next';
+import { Dropdown } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 
 export default function MainNavbar() {
+  const auth = getCookie('auth');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const logout = () => {
+    deleteCookie('auth');
+    alert('로그아웃 되었습니다.');
+    setIsLogin(false);
+  }
+
+  useEffect(() => {
+  }, [isLogin]);
+
   return <NavbarWrap>
     <Navbar>
       <Icon>
@@ -11,12 +26,32 @@ export default function MainNavbar() {
         <IconText>Live Pulse</IconText>
       </Icon>
       <Logo>
-        <Link href="/broadcast/create">
+        <Link href="/broadcast/create" style={{width: "30px", height: "30px"}}>
           { plusIcon({width: 30, height: 30}) }
         </Link>
-        <Link href="/users">
-          { userIcon({width: 30, height: 30}) }
-        </Link>
+        { !auth &&
+          <Link href="/users" style={{width: "30px", height: "30px"}}>
+            { userIcon({width: 30, height: 30}) }
+          </Link>
+        }
+        { auth &&
+          <Dropdown style={{padding: "0% 3% 0% 3%"}}>
+            <Dropdown.Button light color="error">
+              { userIcon({width: 30, height: 30}) }
+            </Dropdown.Button>
+            <Dropdown.Menu
+              variant="light"
+              aria-label="Actions"
+            >
+              <Dropdown.Item key="edit">
+                <Link href="/users/my-page">MyPage</Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="delete" color="error">
+                <a onClick={logout}>Logout</a>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        }
       </Logo>
     </Navbar>
     <Menubar />
@@ -57,7 +92,14 @@ const IconText = styled.div`
 
 const Logo = styled.div`
   padding: 3% 3% 2% 2%;
-  svg {
-    margin-left: 5px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  button {
+    padding: 0 5% 0 5% !important;
+  }
+  span {
+    margin: 0 !important;
   }
 `;
